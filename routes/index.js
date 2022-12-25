dd34576f5b7ac01f5ee7dd436ab36c12fd70c99e
Alberto Valenzuela <avalen28@gmail.com>
2022-12-25 20:51:57 +0100

    adds route and view of movie details. adds .catch to routes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const router = express.Router();
+
 const Movie = require("../models/Movie.model");
 
 /* GET home page */
 router.get("/", (req, res, next) => res.render("index"));
 
+/* Movies page */
 router.get("/movies", (req, res, next) => {
-  Movie.find({}).then((moviesFromDB) => res.render("movies", { moviesFromDB }));
+  Movie.find({})
+    .then((moviesFromDB) => res.render("movies", { moviesFromDB }))
+    .catch((err) => next(err));
+});
+
+/* Movie details page */
+router.get("/movie/:id", (req, res) => {
+  const movieId = req.params.id;
+  Movie.findById(movieId).then(
+    (movieFromDB) =>
+      res.render(
+        "movie-detail",
+        movieFromDB
+      ) /*no se pone curly porque al ser 1 sólo ya es un obj. */
+  );
 });
 
 //last line of code always
